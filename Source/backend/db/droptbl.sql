9bf3f0f943d3fecf9650ff5bfd2c7207d6321909
ameme12 <155856521+ameme12@users.noreply.github.com>
2024-12-13 12:46:12 -0500

    Update createtbl.sql
    finished - no testing

---
+++ Source/backend/db/droptbl.sql
@@ -0,0 +1,13 @@
+DROP TABLE User;
+DROP TABLE Members;
+DROP TABLE Booking;
+DROP TABLE Poll;
+DROP TABLE Notification;
+DROP TABLE Attachments;
+DROP TABLE Days;
+DROP TABLE AvailableSLots;
+DROP TABLE Dates;
+DROP TABLE Reserve;
+DROP TABLE Votes;
+DROP TABLE BookingAttachment;
+
