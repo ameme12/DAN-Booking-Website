9e0bca126c81c35aeaf6e282a997a7e14a6404c0
ameme12 <ameline.ramesan@icloud.com>
2024-12-14 10:50:31 -0500

    Update createtbl.sql
    finished - no testing

--- Source/backend/db/droptbl.sql
+++ Source/backend/db/droptbl.sql
@@ -4,10 +4,10 @@ DROP TABLE Booking;
 DROP TABLE Poll;
 DROP TABLE Notification;
 DROP TABLE Attachments;
-DROP TABLE Days;
 DROP TABLE AvailableSLots;
 DROP TABLE Dates;
 DROP TABLE Reserve;
 DROP TABLE Votes;
 DROP TABLE BookingAttachment;
+DROP TABLE NotificationTemplate;
 
